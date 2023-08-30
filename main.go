0f85ad1e7bc0a44ea37eea64eb6e8aef6f9146cc
Evan Fadhilla <evfadhw@gmail.com>
2023-08-30 17:45:51 +0700

    product crud added

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"CRUD/config"
 	"CRUD/controllers/categorycontroller"
 	"CRUD/controllers/homecontroller"
+	"CRUD/controllers/productcontroller"
 	"log"
 	"net/http"
 )
@@ -20,6 +21,12 @@ func main() {
 	http.HandleFunc("/categories/edit", categorycontroller.Edit)
 	http.HandleFunc("/categories/delete", categorycontroller.Delete)
 
+	// Product
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
 
 	log.Println("Server started on: http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
